4695798ded25b1198c20cf1afc2c7ba64024562a
Adrian Vasile <85063518+AdyRo206@users.noreply.github.com>
2023-12-19 13:57:29 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Feb 13, 2024 14:50:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
