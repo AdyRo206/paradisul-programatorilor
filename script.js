d0ea5eb5f7b407407b5cfea2318cdfb10355c2a9
Adrian <adriangabrielv2007@gmail.com>
2024-05-20 20:42:02 +0300

    Updates

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var countDownDate = new Date("Feb 13, 2024 14:50:25").getTime();
+let countDownDate = new Date("Feb 13, 2024 14:50:25").getTime();
 
-var x = setInterval(function() {
+let x = setInterval(function() {
 
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
 
-  var distance = countDownDate - now;
+  let distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("timer").innerHTML = days + " zile " + hours + " ore "
   + minutes + " minute " + seconds + " secunde ";
@@ -20,3 +20,4 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "Site-ul nostru va fi gata in curand";
   }
 }, 1000);
+
